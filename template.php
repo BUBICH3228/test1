5f08f4378564507c352ea3c7a2e6e4c02c93a0a6
BUBICH <79073551+BUBICH3228@users.noreply.github.com>
2025-11-12 11:09:44 +0700

    -

--- template.php
+++ template.php
@@ -1,46 +1,103 @@
 <?php
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-/** @var array $arParams */
-/** @var array $arResult */
-$this->setFrameMode(true);
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
 
-$this->addExternalCss($this->GetFolder().'/css/common.css');
+$this->addExternalCss($this->GetFolder() . '/commonsss');
+
 ?>
-
-<div id="barba-wrapper">
-    <div class="article-list">
-        <?php foreach ($arResult["ITEMS"] as $arItem): ?>
-            <?
-            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            
-            <a class="article-item article-list__item" href="<?= $arItem['DETAIL_PAGE_URL'] ?>" data-anim="anim-3" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                <?php if ($arParams["DISPLAY_PICTURE"] != "N"): ?>
-                    <div class="article-item__background">
-                        <?php if (is_array($arItem["PREVIEW_PICTURE"])): ?>
-                            <img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>" />
-                        <?php else: ?>
-                            <img src="<?= $this->GetFolder() ?>/images/article-item-bg-1.jpg" alt="<?= $arItem["NAME"] ?>" />
-                        <?php endif; ?>
-                    </div>
-                <?php endif; ?>
-                
-                <div class="article-item__wrapper">
-                    <?php if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
-                        <div class="article-item__title"><?= $arItem["NAME"] ?></div>
-                    <?php endif; ?>
-                    
-                    <?php if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
-                        <div class="article-item__content"><?= $arItem["PREVIEW_TEXT"] ?></div>
-                    <?php endif; ?>
-                </div>
-            </a>
-        <?php endforeach; ?>
+<div class="contact-form">
+    <div class="contact-form__head">
+        <div class="contact-form__head-title">Связаться</div>
+        <div class="contact-form__head-text">
+            Наши сотрудники помогут выполнить подбор услуги и&nbsp;расчет цены с&nbsp;учетом ваших требований
+        </div>
     </div>
+    <form class="contact-form__form" name="<?= $arResult['WEB_FORM_NAME'] ?>" action="<?= $arResult['FORM_ACTION'] ?>" method="POST" enctype="multipart/form-data">
+        <?= $arResult['sessid'] ?>
+        <input type="hidden" name="WEB_FORM_ID" value="<?= $arResult['WEB_FORM_ID'] ?>">
+        
+        <?php if ($arResult['isFormErrors'] === 'Y'): ?>
+            <div class="alert"><?= $arResult['FORM_ERRORS_TEXT'] ?></div>
+        <?php endif; ?>
+        
+        <?php if ($arResult['isFormNote'] === 'Y'): ?>
+            <div class="success"><?= $arResult['FORM_NOTE'] ?></div>
+        <?php endif; ?>
+        
+        <?php if ($arResult['isFormFields'] === 'Y'): ?>
+        <div class="contact-form__form-inputs">
+            <?php foreach ($arResult['QUESTIONS'] as $FIELD_SID => $question): ?>
+                <?php if ($FIELD_SID === 'NAME'): ?>
+                <div class="input contact-form__input">
+                    <label class="input__label">
+                        <div class="input__label-text">Ваше имя*</div>
+                        <?= $question['HTML_CODE'] ?>
+                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                    </label>
+                </div>
+                <?php elseif ($FIELD_SID === 'COMPANY'): ?>
+                <div class="input contact-form__input">
+                    <label class="input__label">
+                        <div class="input__label-text">Компания/Должность*</div>
+                        <?= $question['HTML_CODE'] ?>
+                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                    </label>
+                </div>
+                <?php elseif ($FIELD_SID === 'EMAIL'): ?>
+                <div class="input contact-form__input">
+                    <label class="input__label">
+                        <div class="input__label-text">Email*</div>
+                        <?= $question['HTML_CODE'] ?>
+                        <div class="input__notification">Неверный формат почты</div>
+                    </label>
+                </div>
+                <?php elseif ($FIELD_SID === 'PHONE'): ?>
+                <div class="input contact-form__input">
+                    <label class="input__label" for="medicine_phone">
+                        <div class="input__label-text">Номер телефона*</div>
+                        <input 
+                            class="input__input" 
+                            type="tel" 
+                            id="medicine_phone"
+                            name="<?= $question['FIELD_NAME'] ?>"
+                            data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" 
+                            maxlength="12"
+                            x-autocompletetype="phone-full" 
+                            value="<?= $question['VALUE'] ?>" 
+                            required
+                        >
+                    </label>
+                </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+        
+        <?php if (isset($arResult['QUESTIONS']['MESSAGE'])): ?>
+        <div class="contact-form__form-message">
+            <div class="input">
+                <label class="input__label">
+                    <div class="input__label-text">Сообщение</div>
+                    <?= $arResult['QUESTIONS']['MESSAGE']['HTML_CODE'] ?>
+                    <div class="input__notification"></div>
+                </label>
+            </div>
+        </div>
+        <?php endif; ?>
+        
+        <div class="contact-form__bottom">
+            <div class="contact-form__bottom-policy">
+                Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку персональных данных&raquo;.
+            </div>
+            <input 
+                type="submit" 
+                name="web_form_submit" 
+                value="<?= strlen($arResult['arForm']['BUTTON']) > 0 ? $arResult['arForm']['BUTTON'] : 'Оставить заявку' ?>" 
+                class="form-button contact-form__bottom-button"
+            />
+            <input type="hidden" name="web_form_apply" value="Y">
+        </div>
+        <?php endif; ?>
+    </form>
 </div>
-
-<?php if (empty($arResult["ITEMS"])): ?>
-    <p>Новостей не найдено.</p>
-<?php endif; ?>
